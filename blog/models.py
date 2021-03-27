1330bb1c23869cb4c0a61b704e172f3d1bcd48f8
divyamani <divyamani.arya@inviz.ai>
2021-03-27 13:08:33 +0530

    unit test for model post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=32)
+
+    def __str__(self):
+        return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTesting(TestCase):
+
+    def setUp(self):
+        self.blog = Post.objects.create(title="Django",
+                                        author="Ramesh", slug="Dj")
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), "Django")
